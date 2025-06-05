a5c335ec9a21fe2e4b75d11e639dcf76cd8e3ff5
Brennan Horvath <bhorvath@sgi.sk.ca>
2025-06-05 16:45:46 -0600

    done with css.... for now

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,27 +1,30 @@
 export default function MainPage() {
     return (
-        <div className="col">
-       <div className="container text-center">
-            <div className="row">
-                <div className="col div-style">
-               <img src="../me.jpg" alt="picture of me :)"></img>
+        <div className="col parent-div">
+            <div className="container text-center" style={{ minHeight: '40vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <div className="row w-100 align-items-center justify-content-center">
+                    <div className="col-lg-4 d-flex justify-content-center align-items-center div-style-2" style={{ maxHeight: '40vh' }}>
+                        <img className="me" src="../me.jpg" alt="me :)" style={{ maxHeight: '40vh', minHeight: '40vh', minWidth: '40vh' }}/>
+                    </div>
+                    <div className="col-lg-5 d-flex flex-column align-items-center justify-content-center text-center div-style" style={{ minHeight: '40vh' }}>
+                        <h2>Hello, I'm Brennan.</h2>
+                        <p className="main-p">Computer Science Student at the University of Regina</p>
+                        <p className="main-p">Currently: Work Term with SGI 🚗</p>
+                    </div>
                 </div>
-                <div className="col d-flex flex-column align-items-center justify-content-center text-center div-style" style={{ minHeight: '20vh' }}>
-                <h2>Hello, I'm Brennan.</h2>
-                <p className="main-p">Computer Science Student at the University of Regina</p>
-                <p className="main-p">Currently: Work Term with SGI 🚗</p>
- 
+            </div>
+            <div className="container text-center mt-4" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <div className="row w-100 align-items-center justify-content-center">
+                    <div className="col-lg-5 d-flex flex-column align-items-center justify-content-center text-center div-style" style={{ minHeight: '30vh' }}>
+                        <h5>About Me</h5>
+                        <p>22 year old fourth year student, average of 89%. I like "trying" coding. </p>
+                    </div>
+                    <div className="col-lg-4 d-flex flex-column align-items-center justify-content-center text-center div-style" style={{ minHeight: '30vh' }}>
+                        <h2>Technologies</h2>
+                        <p className="main-p"></p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div className="d-flex justify-content-center">
-            <div className="col d-flex flex-column align-items-center justify-content-center text-center div-style-2">
-                <h5>About Me</h5>
-                <p>22 year old fourth year student, average of 89%.
-                    I like to "try" coding. </p>
-            </div>
-        </div>
-        </div>
-    )
-    
+    );
 }

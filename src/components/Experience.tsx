e9576f653076b8317f88aafab322d14d293df2a7
Brennan <brennanhorvath@icloud.com>
2026-01-21 14:48:59 -0600

    add styling to Experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -62,16 +62,17 @@ function Experience() {
         <div className="col-lg-2 d-flex flex-column justify-content-center text-center div-style-3 mb-2 py-2" style={{ minHeight: '10vh', minWidth: '20vh' }}>
           <h2 className="m-0"data-usal="text-fluid split-letter duration-1750 split-delay-75">Experience</h2>
         </div>
-        <div className="col-lg-2 d-flex flex-column justify-content-center text-center div-style-3 my-2" style={{ minHeight: '30vh', minWidth: '50vh' }}>
+        <div className="col-lg-2 d-flex flex-column justify-content-center div-style-3 my-2 text-center" style={{ minHeight: '30vh', minWidth: '70vh' }}>
           <h4 data-usal="text-fluid split-letter duration-1750 split-delay-75">Currently:</h4>
-          <p>Building a Blackjack game with a Balatro Twist in C++, implementing Lua asap.</p>
+          <p className='px-2'>Adding onto this site. Using ThreeJS and Blender for ASCII art. <br></br>
+            Building a Blackjack game with a Balatro Twist in C++, implementing Lua asap.</p>
         </div>
-        <div className="col-lg-2 d-flex flex-column justify-content-center text-center div-style-3 my-2 px-2" style={{ minHeight: '30vh', minWidth: '50vh' }}>
+        <div className="col-lg-2 d-flex flex-column justify-content-center text-center div-style-3 my-2 px-2" style={{ minHeight: '30vh', minWidth: '70vh' }}>
           <h4 data-usal="text-fluid split-letter duration-1750 split-delay-75">May 2025 - Aug 2025</h4>
           <h5><b>Developer Intern @ <br></br>Saskatchewan Government Insurance</b></h5>
           <p>C#, TypeScript/JavaScript, Java (legacy system integrations), React, Jest, Git, Zustand, Azure DevOps, Agile/Scrum, REST APIs</p>
         </div>
-        <div className="col-lg-2 d-flex flex-column justify-content-center text-center div-style-3 my-2 px-2" style={{ minHeight: '30vh', minWidth: '50vh' }}>
+        <div className="col-lg-2 d-flex flex-column justify-content-center text-center div-style-3 my-2 px-2" style={{ minHeight: '30vh', minWidth: '70vh' }}>
           <h4 data-usal="text-fluid split-letter duration-1750 split-delay-75">May 2024 - Apr 2025</h4>
           <h5><b>Configuration Technician Intern @ Government of Saskatchewan</b></h5>
           <p>SQL, Microsoft Excel, Visual Basic, Reporting systems, Hardware/OS troubleshooting</p>
